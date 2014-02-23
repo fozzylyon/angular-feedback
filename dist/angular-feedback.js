78127a4f43380a65392c694d6258a563f8b4e2e9
James Lyon <fozzylyon@gmail.com>
2014-02-23 13:06:46 -0700

    Added delayed close with successMessage (default=2000ms)

--- dist/angular-feedback.js
+++ dist/angular-feedback.js
@@ -1,12 +1,13 @@
 ( function ( ) {
 	angular.module( 'angular-feedback', [ ] )
-		.directive( 'feedbackWidget', [ '$http', '$window',
-			function ( $http, $window ) {
+		.directive( 'feedbackWidget', [ '$http', '$window', '$timeout',
+			function ( $http, $window, $timeout ) {
 				return {
 					'restrict': 'E',
 					'scope': {
 						'url': '@',
-						'successMessage': '@'
+						'successMessage': '@',
+						'successDelay': '@?'
 					},
 
 					'link': function ( scope, element, attrs, controller ) {
@@ -19,7 +20,11 @@
 						scope.submitFeedback = function ( ) {
 							$http.post( scope.url, {
 								'feedback': scope.feedback
-							} );
+							} )
+								.success( function ( foods ) {
+									scope.success = true;
+									$timeout( scope.close, scope.successDelay || 2000 );
+								} );
 						};
 
 						scope.close = function ( ) {
@@ -45,7 +50,7 @@
 							}
 						} );
 					},
-					'template': "<div>" + "<div class='overlay' ng-show='open'></div>" + "<div class='mod' ng-show='open'>" + "<div class='modal-dialog'>" + "<div class='modal-content'>" + "<div class='modal-header'>" + "<button type='button' class='close' ng-click='close()'>&times;</button>" + "<h4 class='modal-title'>Feedback</h4>" + "</div>" + "<div class='modal-body'>" + "<form name='feedback' role='form' class='form-horizontal' ng-model='feedback.comment' ng-submit='submitFeedback()''>" + "<div class='form-group'>" + "<label class='control-label sr-only' for='feedbackInput'>Feedback</label>" + "<textarea id='feedbackInput' class='form-control' row='3' ng-model='feedback.comment' placeholder='Leave any feedback'>" + "</textarea>" + "</div>" + "</div>" + "<div class='modal-footer'>" + "<button class='btn btn-primary' type='submit'>Submit</button>" + "<button class='btn btn-default' type='button' ng-click='close()'>Cancel</button>" + "</div>" + "</form>" + "</div>" + "</div>" + "</div>" + "<button ng-click='open=!open;' class='feedback-launch btn btn-success'>Feedback</button>" + "</div>"
+					'template': "<div>" + "<div class='overlay' ng-show='open'></div>" + "<div class='mod' ng-show='open'>" + "<div class='modal-dialog'>" + "<div class='modal-content'>" + "<div class='modal-header'>" + "<button type='button' class='close' ng-click='close()'>&times;</button>" + "<h4 class='modal-title'>Feedback</h4>" + "</div>" + "<div class='modal-body'>" + "<form name='feedback' role='form' class='form-horizontal' ng-model='feedback.comment' ng-submit='submitFeedback()''>" + "<div class='form-group'>" + "<label class='control-label sr-only' for='feedbackInput'>Feedback</label>" + "<textarea ng-show='!success' id='feedbackInput' class='form-control' row='3' ng-model='feedback.comment' placeholder='Leave any feedback'>" + "</textarea>" + "<p ng-show='success'>{{successMessage}}<p>" + "</div>" + "</div>" + "<div class='modal-footer'>" + "<button class='btn btn-primary' type='submit'>Submit</button>" + "<button class='btn btn-default' type='button' ng-click='close()'>Cancel</button>" + "</div>" + "</form>" + "</div>" + "</div>" + "</div>" + "<button ng-click='open=!open;' class='feedback-launch btn btn-success'>Feedback</button>" + "</div>"
 				};
 			}
 		] );
